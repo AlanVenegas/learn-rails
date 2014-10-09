4f9b2ec23e78355dd45ec4e95715be0c291d27f6
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-09 11:54:25 +0200

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,7 @@
+module ApplicationHelper
+
+  def greeting(name)
+    "hi there, #{name}!"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestaurantApp::Application.config.secret_key_base = '2e934c7771c1e99dbaa47e909a2c7a43df784c7aa43927a874acb31f0c66653cd8d71c96bf303e22f91d199f0f9e70b04e477137e5cf2f5ac8a018ff9f9e1c63'
